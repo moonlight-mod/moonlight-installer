35a88022636f4c9e0c3c3081c324ac6fc0128c12
NotNite <hi@notnite.com>
2024-10-05 21:27:19 -0400

    Abandon musl for now

--- build-linux.sh
+++ build-linux.sh
@@ -1,19 +1,21 @@
+#!/usr/bin/env sh
+set -e
+
+cargo build --release
+
+if [ -d ./appdir ]; then
+  rm -rf ./appdir
+fi
+mkdir appdir
+
+cp ./assets/icon.png ./appdir
+cp ./target/release/moonlight-installer ./appdir/AppRun
+cp ./assets/moonlight-installer.desktop ./appdir
+
 if [ ! -f ./appimagetool ]; then
   echo "Downloading appimagetool..."
   wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-$(uname -m).AppImage -O ./appimagetool
   chmod +x ./appimagetool
 fi
-export PATH=$PATH:$(pwd)
 
-if [ -d ./target/moonlight-installer.AppDir ]; then
-  rm -rf ./target/moonlight-installer.AppDir
-fi
-
-if [ -d ./target/appimage ]; then
-  rm -rf ./target/appimage
-fi
-
-cargo build --release --target x86_64-unknown-linux-musl
-# ???
-cp ./target/x86_64-unknown-linux-musl/release/moonlight-installer ./target/release/moonlight-installer
-cargo appimage --target x86_64-unknown-linux-musl
+./appimagetool ./appdir moonlight-installer-x86_64.AppImage
